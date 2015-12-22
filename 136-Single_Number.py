50af0d1f5a8f77f6434e0f4e814ce71b70528d8c
HankChow <hankchow@hankchow.net>
2015-12-22 13:55:03 +0800

    add #136 Single Number

---
+++ 136-Single_Number.py
@@ -0,0 +1,11 @@
+class Solution(object):
+	def singleNumber(self, nums):
+		"""
+		type nums: List[int]
+		rtype: int
+		"""
+		nums = sorted(nums)
+		for i in range(0, (len(nums)-1)/2):
+			if nums[i*2] != nums[i*2+1]:
+				return nums[i*2]
+		return nums[len(nums)-1]
